f4bbf98d98f02cef0e006d8218b3d1a3265e59d9
Jeff Yang <jho.yang96@gmail.com>
2021-11-27 15:56:40 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+/*
+ *    main.js
+ */
+
+// global variables
+let allCalls;
+let calls;
+let nestedCalls;
+let donut;
+let revenueBar;
+let durationBar;
+let unitBar;
+let stackedArea;
+let timeline;
+let scatterPlot;
+
+const parseTime = d3.timeParse("%d/%m/%Y");
+const formatTime = d3.timeFormat("%d/%m/%Y");
+
+d3.csv("data/tech_salary_data.csv").then((data) => {
+  data.forEach((d) => {
+    d.totalyearlycompensation = Number(d.totalyearlycompensation);
+    d.yearsofexperience = Number(d.yearsofexperience);
+    d.yearsatcompany = Number(d.yearsatcompany);
+    d.timestamp = parseTime(d.timestamp);
+    d.basesalary = Number(d.basesalary);
+    d.stockgrantvalue = Number(d.stockgrantvalue);
+    d.bonus = Number(d.bonus);
+  });
+  console.log("data: ", data);
+
+  allCalls = data;
+  // calls = data;
+  scatterPlot = new ScatterPlot("#stacked-area");
+
+  // nestedCalls = d3
+  //   .nest()
+  //   .key((d) => d.category)
+  //   .entries(calls);
+
+  // donut = new DonutChart("#company-size");
+  // revenueBar = new BarChart(
+  //   "#revenue",
+  //   "call_revenue",
+  //   "Average call revenue (USD)"
+  // );
+  // durationBar = new BarChart(
+  //   "#call-duration",
+  //   "call_duration",
+  //   "Average call duration (seconds)"
+  // );
+  // unitBar = new BarChart("#units-sold", "units_sold", "Units sold per call");
+  // stackedArea = new StackedAreaChart("#stacked-area");
+  // timeline = new Timeline("#timeline");
+});
+
+// $("#var-select").on("change", () => {
+//   stackedArea.wrangleData();
+//   timeline.wrangleData();
+// });
+
+function brushed() {
+  const selection = d3.event.selection || timeline.x.range();
+  const newValues = selection.map(timeline.x.invert);
+  changeDates(newValues);
+}
+
+function changeDates(values) {
+  calls = allCalls.filter((d) => d.date > values[0] && d.date < values[1]);
+
+  nestedCalls = d3
+    .nest()
+    .key((d) => d.category)
+    .entries(calls);
+
+  $("#dateLabel1").text(formatTime(values[0]));
+  $("#dateLabel2").text(formatTime(values[1]));
+
+  donut.wrangleData();
+  revenueBar.wrangleData();
+  unitBar.wrangleData();
+  durationBar.wrangleData();
+  stackedArea.wrangleData();
+}
---
+++ js/scatterPlot.js
@@ -0,0 +1,70 @@
+/*
+ *    scatterPlot.js
+ */
+
+class ScatterPlot {
+  constructor(_parentElement) {
+    this.parentElement = _parentElement;
+
+    this.initVis();
+  }
+
+  initVis() {
+    const vis = this;
+
+    vis.MARGIN = { LEFT: 80, RIGHT: 100, TOP: 50, BOTTOM: 40 };
+    vis.WIDTH = 800 - vis.MARGIN.LEFT - vis.MARGIN.RIGHT;
+    vis.HEIGHT = 370 - vis.MARGIN.TOP - vis.MARGIN.BOTTOM;
+
+    vis.svg = d3
+      .select(vis.parentElement)
+      .append("svg")
+      .attr("width", vis.WIDTH + vis.MARGIN.LEFT + vis.MARGIN.RIGHT)
+      .attr("height", vis.HEIGHT + vis.MARGIN.TOP + vis.MARGIN.BOTTOM);
+
+    vis.g = vis.svg
+      .append("g")
+      .attr("transform", `translate(${vis.MARGIN.LEFT}, ${vis.MARGIN.TOP})`);
+
+    vis.color = d3.scaleOrdinal(d3.schemePastel1);
+
+    vis.x = d3.scaleLinear().domain([0, 40]).range([0, vis.WIDTH]);
+    vis.y = d3.scaleLinear().domain([0, 2000000]).range([vis.HEIGHT, 0]);
+
+    vis.yAxisCall = d3.axisLeft(vis.y);
+    vis.xAxisCall = d3.axisBottom(vis.x).ticks();
+    vis.xAxis = vis.g
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0, ${vis.HEIGHT})`)
+      .call(vis.xAxisCall);
+    vis.yAxis = vis.g.append("g").attr("class", "y axis").call(vis.yAxisCall);
+
+    // Add dots
+    const dots = vis.g.selectAll("dot").data(allCalls);
+    console.log("dots: ", dots);
+    dots
+      .enter()
+      .append("circle")
+      .attr("cx", (d) => vis.x(d.yearsofexperience))
+      .attr("cy", (d) => vis.y(d.totalyearlycompensation))
+      .attr("r", 1.5)
+      .attr("fill", "#69b3a2");
+
+    // vis.addLegend();
+
+    vis.wrangleData();
+  }
+
+  wrangleData() {
+    // wrangle data
+  }
+
+  updateVis() {
+    // update vis
+  }
+
+  addLegend() {
+    // add legend
+  }
+}
